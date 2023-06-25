6ac0e55fb332771f86ae29467f7cea7d0cae6c27
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-06-25 11:42:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-  )
+  );
 }
 
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 🤩 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have x items on your list and you already packed x (x%)</em>
+    </footer>
+  );
+}
