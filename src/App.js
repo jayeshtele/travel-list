dc4f0f3f1b0b8287fbb783ec85e5e1607d3107ad
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-06-25 12:48:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,14 +23,23 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    setDescription("");
+    setQuantity("");
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 🤩 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
